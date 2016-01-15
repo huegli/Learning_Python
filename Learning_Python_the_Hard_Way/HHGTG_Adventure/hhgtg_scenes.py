64f5f2e0ae37228430163ef6987e3be8908a48f3
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-01-14 17:52:49 -0800

    Start of HHGTG Adventure

---
+++ Learning_Python_the_Hard_Way/HHGTG_Adventure/hhgtg_scenes.py
@@ -0,0 +1,9 @@
+bedroom_dict = {
+    'intro':  """
+You wake up with a terrible hangover. As you try to get up the world spins -
+around you and you feel even more sick.
+"""
+    ,'sleep':  """
+You go back to sleep. Shortly afterwards, the earth is destroyed by the Vogons
+"""
+}
---
+++ Learning_Python_the_Hard_Way/HHGTG_Adventure/hhgtg_scenes_test.py
@@ -0,0 +1,4 @@
+import hhgtg_scenes
+
+print hhgtg_scenes.bedroom_dict['intro']
+print hhgtg_scenes.bedroom_dict['sleep']
