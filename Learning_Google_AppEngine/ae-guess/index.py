500a5c4e0035063b09bc4129b26fcf05345fdbe3
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-03-28 15:49:23 +0000

    First Version

---
+++ Learning_Google_AppEngine/ae-guess/index.py
@@ -0,0 +1,30 @@
+import sys
+
+print 'Content-Type: text/html'
+print ''
+
+print '<pre>'
+
+# Read the form input which is a single line
+# guess=25
+guess = -1
+data = sys.stdin.read()
+# print data
+try:
+    guess = int(data[data.find('=')+1:])
+except:
+    guess = -1
+print 'Your guess is', guess
+answer = 42;
+if guess < answer:
+    print 'Your guess is too low'
+if guess == answer:
+    print 'Congratulations!'
+if guess > answer:
+    print 'Your guess is too high'
+print '</pre>'
+
+print '''<form method="post" action="/">
+<p>Enter Guess: <input type="text" name="guess"></p>
+<p><input type="submit"></p>
+</form>'''
