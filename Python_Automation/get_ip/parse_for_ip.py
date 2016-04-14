f80cc5f56b2a9f2e4045f0ee971cf1151c319cb8
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-04-14 08:21:21 -0700

    Second version with while

--- Python_Automation/get_ip/parse_for_ip.py
+++ Python_Automation/get_ip/parse_for_ip.py
@@ -4,8 +4,12 @@ import sys
 import re
 
 lines = sys.stdin.readlines()
-ip_line = lines[105]
+# ip_line = lines[105]
 
-match = re.search('<td>(.+?)</td>', ip_line)
+id=0
+while not ('IP Address' in lines[id]):
+    id = id + 1
+
+match = re.search('<td>(.+?)</td>', lines[id+1])
 if match:
     print match.group(1)
