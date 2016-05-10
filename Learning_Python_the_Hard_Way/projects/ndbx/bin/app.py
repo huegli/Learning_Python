e472ba8aef73abc1485b785db470b3ac2e087d04
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-05-10 22:41:09 +0000

    Updated ndbx

--- Learning_Python_the_Hard_Way/projects/ndbx/bin/app.py
+++ Learning_Python_the_Hard_Way/projects/ndbx/bin/app.py
@@ -1,4 +1,15 @@
 import web
+import cgi
+import os
+
+# Maximum input of 1 MB for POST
+cgi.maxlen = 1024 * 1024; # 1 MB
+
+# remove previous pic
+have_pic = False
+if os.path.isfile("./static/picture.jpg"):
+    os.remove("./static/picture.jpg")
+
 
 urls = (
         '/', 'Index',
@@ -11,19 +22,31 @@ render = web.template.render('templates/', base="layout")
 
 class Index(object):
     def GET(self):
-        return render.index()
+        return render.index(have_pic=have_pic)
 
 class Upload(object):
     def GET(self):
         return render.upload()
 
     def POST(self):
-        form = web.input(picfile={}, scale="100%")
-        if 'picfile' in form:
-            fout = open("./static/picture.jpg",'w')
-            fout.write(form.picfile.file.read())
-            fout.close()
-        return render.show(pic = form['picfile'].filename, scale = form.scale)
+        global have_pic
+        try:    
+            form = web.input(picfile={}, scale="100%")
+        except ValueError:
+            return "File too large"
+        else: 
+            if 'picfile' in form:
+                filepath=form.picfile.filename.replace('\\','/') # Windows -> Linux slashes
+                filename=filepath.split('/')[-1] # pick up last part (filename & extension
+                fileext=filename.split('.')[-1] # extension
+                if 'jpg' in fileext:
+                    fout = open("./static/picture.jpg",'w')
+                    fout.write(form.picfile.file.read())
+                    fout.close()
+                    have_pic = True
+                    raise web.seeother('/')
+                else:
+                    return "JPG's only"
 
 if __name__ == "__main__":
     app.run()
