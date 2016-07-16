69846bcceedcb78ca0bcc956255d143d7cf4c0df
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-07-15 22:34:59 -0700

    Updates

--- The_Python_Book/Python_Essentials/pokerdice.py
+++ The_Python_Book/Python_Essentials/pokerdice.py
@@ -1,7 +1,50 @@
 #!/usr/bin/env python2
 
+from Tkinter import *
+from ttk import *
 import random
 from itertools import groupby
+dice = 0
+
+def roll(roll_number):
+    numbers = range(1,7)
+    dice = range(roll_number)
+    iterations = 0
+    while iterations < roll_number:
+        iterations = iterations + 1
+        dice[iterations-1] = random.choice(numbers)
+    return dice
+
+def hand(dice):
+    dice_hand = [len(list(group)) for key, group in groupby(dice)]
+    dice_hand.sort(reverse=True)
+    straight1 = [1,2,3,4,5]
+    straight2 = [2,3,4,5,6]
+    if dice == straight1 or dice == straight2:
+        return "a straight"
+    elif dice_hand[0] == 5:
+        return "five of a kind!"
+    elif dice_hand[0] == 4:
+        return "four of a kind!"
+    elif dice_hand[0] == 3:
+        if dice_hand[1] == 2:
+            return "a full house!"
+        else:
+            return "three of a kind!"
+    elif dice_hand[0] == 2:
+        if dice_hand[1] == 2:
+            return "two pair"
+        else:
+            return "two of a kind"
+    else:
+        return "a high card."
+        
+def gui():
+    global dice    
+
+if __name__ == '__main__':
+    start()
+
 
 nine = 1
 ten = 2
