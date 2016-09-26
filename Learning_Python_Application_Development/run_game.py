817092905c108933b93aefbca12578b5a95a0dda
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-09-25 20:55:56 -0700

    Making it a module

---
+++ Learning_Python_Application_Development/run_game.py
@@ -0,0 +1,4 @@
+from wargame.attackoftheorcs import AttackOfTheOrcs
+
+game = AttackOfTheOrcs()
+game.play()
---
+++ Learning_Python_Application_Development/wargame/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import os
+import sys
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_path)
+# optionally, print the sys.path for debugging
+# print("in __init__.py sys.path:\n ",sys.path)
