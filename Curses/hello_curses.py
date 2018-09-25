60c80e1f964e0ecc723fb17f8d8156a114eb30fc
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2018-09-24 17:32:12 -0700

    Updates

--- Curses/hello_curses.py
+++ Curses/hello_curses.py
@@ -2,9 +2,11 @@
 
 import curses
 
+
 def main(stdscr):
-    stdscr.addstr(5 ,5 ," Hello World " )
+    stdscr.addstr(5, 5, " Hello World ")
     stdscr.refresh()
     stdscr.getch()
+
 
 curses.wrapper(main)
--- Hello/hello.py
+++ Hello/hello.py
@@ -4,11 +4,11 @@ from __future__ import print_function
 # This program says hello and asks for my name.
 
 print('Hello World!')
-print('What is your name?')	# ask for their name
+print('What is your name?')  # ask for their name
 myName = input()
 print('It is good to meet you, {}'.format(myName))
 print('The length of your name is:')
 print(len(myName))
-print('What is your age?')	# ask for their age
+print('What is your age?')  # ask for their age
 myAge = input()
 print('You will be {} in a year.'.format(str(int(myAge) + 1)))
